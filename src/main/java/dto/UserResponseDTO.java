1d80032cf206808ed018d6961426f0cb2786bff0
antonkormschikov <antonkormschikov@yandex.ru>
2024-01-28 23:25:36 +0300

    28/01/2024

---
+++ src/main/java/dto/UserResponseDTO.java
@@ -0,0 +1,18 @@
+
+package dto;
+
+import javax.annotation.Generated;
+
+import lombok.Data;
+
+@Data
+public class UserResponseDTO {
+
+    private Long code;
+
+    private String message;
+
+    private String type;
+
+
+}
--- src/test/java/CreateNewUserTest.java
+++ src/test/java/CreateNewUserTest.java
@@ -1,4 +1,6 @@
 import dto.UserDTO;
+import io.restassured.module.jsv.JsonSchemaValidator;
+import io.restassured.response.ValidatableResponse;
 import org.apache.http.HttpStatus;
 import org.junit.jupiter.api.Test;
 import services.ServiceApi;
@@ -17,11 +19,26 @@ public class CreateNewUserTest {
                 .email("jack@mail.com")
                 .build();
 
-        userApi.createUser(userDTO)
+       userApi.createUser(userDTO)
                 .statusCode(HttpStatus.SC_OK)
                 .body("code",equalTo(200))
                 .body("type",equalTo("unknown"))
-                .body("message",equalTo("4069"));
+                .body("message",equalTo("4069"))
+                .body(JsonSchemaValidator.matchesJsonSchemaInClasspath("schema/CreateUser.json"))
+       ;
+
+        ValidatableResponse response = userApi.createUser(userDTO);
+
+        //String actualMessage=response.extract().jsonPath().get("message").toString();
+
+       // Assertions.assertEquals("4069",actualMessage,"Incorrect message");
+
+      /*  UserResponseDTO actualUser = response.extract().body().as(UserResponseDTO.class);
+        Assertions.assertAll("Check create user response",
+                () -> Assertions.assertEquals(userDTO.getId().toString(),actualUser.getMessage(),"Incorrect message"),
+                () -> Assertions.assertEquals(200,actualUser.getCode(),"Incorrect code"),
+                () -> Assertions.assertEquals("unknown",actualUser.getType())
+        );*/
 
 
 
