64cc24de7b105759e798d5a93a4118345bca323d
antonkormschikov <antonkormschikov@yandex.ru>
2024-07-26 01:43:55 +0300

    26.07.2024n

--- jenkins/api_test.groovy
+++ jenkins/api_test.groovy
@@ -3,16 +3,20 @@ timeout(60) {
         def testContainerName = "api_tests_$BUILD_NUMBER"
         try {
             stage("Run API tests") {
-                sh "docker run --rm --network=host --name $testContainerName -v $pwd/allure-rezults:/home/anton/target/allure-rezults -t api-tests"
+                sh "docker run --rm --network=host --name $testContainerName -v $pwdpwd/allure-rezults:/home/anton/target/allure-rezults -t api-tests"
             }
             stage("Publish Allure report"){
+
                 allure([
                         disabled: true,
                         results: ["$pwd/allure-results"]
                 ])
             }
             stage("Telegram notification"){
-                sh "curl -s -X POST https://api.telegram.org/bot7460003126:AAGHETwA70UMIQokbKS9_hJaOR7eOeDCWKo/sendMessage -d chat_id=-4272423291 -d text=\"very simple text\""
+                File[] files = new File("$pwd/allure-results").listFiles()
+                for (File file in files){
+                    sh String.format("curl -s -X POST https://api.telegram.org/bot7460003126:AAGHETwA70UMIQokbKS9_hJaOR7eOeDCWKo/sendMessage -d chat_id=-4272423291 -d text=%s",file.getText('UTF-8')
+                }
 
             }
         } finally {
@@ -21,3 +25,4 @@ timeout(60) {
 
     }
 }
+
---
+++ src/test/fileAllureTest.java
@@ -0,0 +1,5 @@
+import java.io.File;
+
+public class fileAllureTest {
+    File[] files = new File("/allure-results/").listFiles()
+}
